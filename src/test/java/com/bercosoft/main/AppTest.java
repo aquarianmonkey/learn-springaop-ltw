05f6f83d9d4e37686f9f60fb229058917f209feb
aquarianmonkey <bcokee@gmail.com>
2017-05-23 17:31:30 +0800

    clean up

--- src/test/java/com/bercosoft/main/AppTest.java
+++ src/test/java/com/bercosoft/main/AppTest.java
@@ -13,7 +13,6 @@ import com.bercosoft.model.Client;
 @ContextConfiguration(locations ={"applicationContext-Test.xml"})
 public class AppTest
 {
-	
 	//Client uut = new Client();  // LTW fails DI here
 	
 	@Test
@@ -21,9 +20,7 @@ public class AppTest
 	{
 		//Used local variable as work around for failed DI on fields with @Configurable
 		Client uut = new Client();
-		
 		assertNotNull("LTW failed to inject spring bean", uut.getGroot());
-		
 	}
 
 }
